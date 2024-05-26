d1ea81a8be579515bbf09af07b109310cc85d6e7
patricktuvera <patricktuvera8@gmail.com>
2024-05-26 12:20:47 +0800

    dash

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('auth.login'); 
+    //return view('welcome');
 });
+  
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
